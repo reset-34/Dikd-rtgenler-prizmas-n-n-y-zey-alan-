092ba705320eb76e23de3d424f62e73c8c6f7ab0
Serap BALCI <81075168+reset-34@users.noreply.github.com>
2024-08-01 15:55:56 +0300

    Add project files.

---
+++ Dikdörtgenler prizmasının yüzey alanı/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dikdörtgenler_prizmasının_yüzey_alanı
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Klavyeden kenarları girilen dikdörtgenler prizmasının yüzey alanını bulan C# programını yazınız.
+                Formül:
+                Yüzey alanı = 2*(a*b+a*c+b*c)            */
+
+
+            Console.WriteLine("a kenarını girin");
+            int a = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("b kenarını girin");
+            int b = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("c kenarını girin");
+            int c = Int32.Parse(Console.ReadLine());
+
+            int yuzeyAlani = 2 * (a * b + a * c + b * c);
+            Console.WriteLine("Dikdörtgenler prizmasının yüzey alanı  = " + yuzeyAlani);
+
+
+            Console.ReadLine();
+        }
+    }
+}
